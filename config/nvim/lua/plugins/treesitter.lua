7cb0fa6e1b2edf75898efc965583a4bdbbb40e7c
Jose Seravalli <jseravalli@gmail.com>
2025-09-28 17:36:02 +0200

    enabling syntax coloring

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,48 @@
 return {
-	"nvim-treesitter/nvim-treesitter", 
-	branch = 'master', 
-	lazy = false, 
-	build = ":TSUpdate"
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- Install parsers automatically
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "bash",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "markdown",
+        "markdown_inline",
+        "rust",
+        "go",
+        "toml",
+      },
+
+      auto_install = true, -- installs missing parsers when you open a file
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+
+      indent = { enable = true },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+    })
+  end,
 }
+
