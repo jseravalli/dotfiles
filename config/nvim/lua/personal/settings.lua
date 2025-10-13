01ddee49aecab0e2cb730fe18ec6ac931b84f4fe
Jose Seravalli <jseravalli@gmail.com>
2025-10-13 14:44:23 +0200

    add special character visualization
    Show tabs, spaces, trailing spaces, line breaks and other special characters in the editor for better code visibility.

--- config/nvim/lua/personal/settings.lua
+++ config/nvim/lua/personal/settings.lua
@@ -2,6 +2,18 @@ vim.opt.number = true -- Show absolute line numbers
 -- Enable truecolor support
 vim.opt.termguicolors = true
 
+-- Show special characters
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "→ ",      -- Show tabs as →
+  space = "·",     -- Show spaces as ·
+  trail = "•",     -- Show trailing spaces as •
+  eol = "↲",       -- Show end of line as ↲
+  extends = "⟩",   -- Show when line continues beyond screen
+  precedes = "⟨",  -- Show when line starts before screen
+  nbsp = "␣",      -- Show non-breaking spaces
+}
+
 -- Global default
 vim.opt.tabstop = 2      -- A Tab character looks like 2 spaces
 vim.opt.shiftwidth = 2   -- >> indents by 2 spaces
