efe98f84b1d21a83e654cbcc562708a297e3042f
Jose Seravalli <jseravalli@gmail.com>
2025-10-14 22:13:37 +0200

    Use official Tokyo Night Moon cyan color for rainbow delimiters
    Update cyan color from #86e1fc to #89ddff to match the official
    Tokyo Night Moon color palette.

--- config/nvim/lua/plugins/indentation.lua
+++ config/nvim/lua/plugins/indentation.lua
@@ -52,7 +52,7 @@ return {
       vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#ff757f" })
       vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#ffc777" })
       vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#c3e88d" })
-      vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#86e1fc" })
+      vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#89ddff" })
       vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#82aaff" })
       vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#c099ff" })
 
